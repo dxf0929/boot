d02bef68df3efab7ba4b1657961cc1c279727e73
dxf <dxf15835220929@163.com>
2020-10-17 21:44:26 +0800

    修改了dao层2.0

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,14 +2,19 @@ package dao;
 
 import entity.User;
 
+import java.util.List;
+
 /*
  *创建日期：2020/10/17/21:37
  *创建人：dxf
  */
 public interface UserDao {
     public User login();
+
     public List<User> queryByList();
+
     public void insert(User user);
+
     public void delete(String id);
     public void update(User user);
 }
