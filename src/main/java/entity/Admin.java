40d980ae1e53d08705838e7b2054f78e4f9fe746
dxf <dxf15835220929@163.com>
2020-10-17 15:18:30 +0800

    图书管理系统基础环境配置1.0

---
+++ src/main/java/entity/Admin.java
@@ -0,0 +1,8 @@
+package entity;
+
+/*
+ *创建日期：2020/10/17/15:17
+ *创建人：dxf
+ */
+public class Admin {
+}
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,8 @@
+package entity;
+
+/*
+ *创建日期：2020/10/17/15:16
+ *创建人：dxf
+ */
+public class User {
+}
